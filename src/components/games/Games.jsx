0d127ebc52f738680444ae6e4053a27455fb780c
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-12 12:00:56 -0500

    Added basic import for FilterBar component in renderer

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -2,18 +2,25 @@ import { Box, Grid } from "@mui/material"
 import { Game } from "./Game.jsx"
 import { useEffect, useState } from "react"
 import { getAllGames } from "../../services/gameService.jsx"
+import { FilterBar } from "./FilterBar.jsx"
 
 export const Games = ({ currentUser }) => {
     const [games, setGames] = useState([])
+    const [filteredGames, setFilteredGames] = useState([])
 
     useEffect(() => {
         getAllGames().then(gamesArray => {
             setGames(gamesArray)
         })
     }, [])
+
+    useEffect(() => {
+        setFilteredGames(games)
+    }, [games])
     
     return (
         <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+            <FilterBar games={games} setFilteredGames={setFilteredGames}/>
             <Box 
                 display={"flex"} 
                 justifyContent={"center"}
@@ -21,7 +28,7 @@ export const Games = ({ currentUser }) => {
                 flexWrap={"wrap"} 
                 gap={4}
             >
-                {games.map(game => {
+                {filteredGames.map(game => {
                     return <Game game={game} currentUser={currentUser} key={game.id} />
                 })}
             </Box>
