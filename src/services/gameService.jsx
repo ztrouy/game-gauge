9083c86c8e5d1ad650f907da9ea6c5abd63ed390
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-12 12:00:19 -0500

    Updated fetch for list of all games to embed genres and expand type

--- src/services/gameService.jsx
+++ src/services/gameService.jsx
@@ -1,3 +1,3 @@
 export const getAllGames = () => {
-    return fetch("http://localhost:8088/games").then(res => res.json())
+    return fetch("http://localhost:8088/games?_embed=gameGenres&_expand=type").then(res => res.json())
 }
