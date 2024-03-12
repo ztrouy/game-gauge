5bf27ffd2f20c91aa0500d39eb97beb36b9364ce
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-12 10:56:26 -0500

    Wrote rendering and logic for displaying list of all games

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -1,10 +1,30 @@
-import { Box } from "@mui/material"
+import { Box, Grid } from "@mui/material"
 import { Game } from "./Game.jsx"
+import { useEffect, useState } from "react"
+import { getAllGames } from "../../services/gameService.jsx"
 
 export const Games = ({ currentUser }) => {
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        getAllGames().then(gamesArray => {
+            setGames(gamesArray)
+        })
+    }, [])
+    
     return (
-        <Box>
-            <Game currentUser={currentUser} />
+        <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+            <Box 
+                display={"flex"} 
+                justifyContent={"center"}
+                flexDirection={"row"} 
+                flexWrap={"wrap"} 
+                gap={4}
+            >
+                {games.map(game => {
+                    return <Game game={game} currentUser={currentUser} key={game.id} />
+                })}
+            </Box>
         </Box>
     )
 }
