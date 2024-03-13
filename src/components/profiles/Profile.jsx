19dde39be17d3514ab0d39d1b01423ef856ac962
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-13 18:57:32 -0500

    Removed unused Box import

--- src/components/profiles/Profile.jsx
+++ src/components/profiles/Profile.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Paper, Typography } from "@mui/material"
+import { Container, Paper, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { getAllGames } from "../../services/gameService.jsx"
 import { GameList } from "../games/GameList.jsx"
