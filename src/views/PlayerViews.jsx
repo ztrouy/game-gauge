0dd2d8b56e11593a7b316a8fd05ada09d52a29c3
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-13 10:56:14 -0500

    Added route for profile view of current user

--- src/views/PlayerViews.jsx
+++ src/views/PlayerViews.jsx
@@ -1,6 +1,7 @@
 import { Outlet, Route, Routes } from "react-router"
 import { Games } from "../components/games/Games.jsx"
 import { NavBar } from "../components/nav/NavBar.jsx"
+import { Profile } from "../components/profiles/Profile.jsx"
 
 export const PlayerViews = ({ currentUser }) => {
     return (
@@ -14,6 +15,9 @@ export const PlayerViews = ({ currentUser }) => {
                 <Route index element={<>Welcome!</>} />
                 <Route path="games">
                     <Route index element={<Games currentUser={currentUser} />} />
+                </Route>
+                <Route path="profile">
+                    <Route index element={<Profile currentUser={currentUser} />} />
                 </Route>
             </Route>
         </Routes>
