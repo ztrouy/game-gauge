47970f6ddb65c5c1db78c0675933016ed79ce0d0
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-13 09:27:16 -0500

    Transferred out rendering of list of games to separate component

--- src/components/games/Games.jsx
+++ src/components/games/Games.jsx
@@ -1,8 +1,8 @@
 import { Box } from "@mui/material"
-import { Game } from "./Game.jsx"
 import { useEffect, useState } from "react"
 import { getAllGames } from "../../services/gameService.jsx"
 import { FilterBar } from "./FilterBar.jsx"
+import { GameList } from "./GameList.jsx"
 
 export const Games = ({ currentUser }) => {
     const [games, setGames] = useState([])
@@ -14,39 +14,11 @@ export const Games = ({ currentUser }) => {
         })
     }, [])
 
-    useEffect(() => {
-        const sortedGames = games.sort((firstGame, secondGame) => {
-            const firstGameName = firstGame.name.toUpperCase()
-            const secondGameName = secondGame.name.toUpperCase()
-
-            if (firstGameName < secondGameName) {
-                return -1
-            } else if (firstGameName > secondGameName) {
-                return 1
-            } else {
-                return 0
-            }
-        })
-
-        setGames(sortedGames)
-        
-        setFilteredGames(games)
-    }, [games])
     
     return (
         <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
             <FilterBar games={games} setFilteredGames={setFilteredGames} currentUser={currentUser} />
-            <Box 
-                display={"flex"} 
-                justifyContent={"center"}
-                flexDirection={"row"} 
-                flexWrap={"wrap"} 
-                gap={4}
-            >
-                {filteredGames.map(game => {
-                    return <Game game={game} currentUser={currentUser} key={game.id} />
-                })}
-            </Box>
+            <GameList games={games} setGames={setGames} filteredGames={filteredGames} setFilteredGames={setFilteredGames} currentUser={currentUser} />
         </Box>
     )
 }
