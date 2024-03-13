4f3bc9b54cd7a9a2c42b24dbb9c884124e9ae5c3
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-13 18:02:46 -0500

    Wrote rendering and logic for profile view

--- src/components/profiles/Profile.jsx
+++ src/components/profiles/Profile.jsx
@@ -1,3 +1,60 @@
+import { Box, Container, Paper, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { getAllGames } from "../../services/gameService.jsx"
+import { GameList } from "../games/GameList.jsx"
+import { useParams } from "react-router"
+import { getUserById } from "../../services/userService.jsx"
+
 export const Profile = ({ currentUser }) => {
-    return (<>Profile View</>)
+    const [games, setGames] = useState([])
+    const [filteredGames, setFilteredGames] = useState([])
+    const [profileUser, setProfileUser] = useState({})
+
+
+
+    const { userId } = useParams()
+
+
+
+    useEffect(() => {
+        let profileId = ""
+        
+        if (userId) {
+            profileId = userId
+        } else {
+            profileId = currentUser.id
+        }
+
+        getUserById(profileId).then(userArray => {
+            const userObject = userArray[0]
+            setProfileUser(userObject)
+        })
+    }, [currentUser])
+
+
+    useEffect(() => {
+        getAllGames().then(gamesArray => {
+            setGames(gamesArray)
+        })
+    }, [])
+    
+
+    useEffect(() => {
+        const ownedGames = games.filter(game => profileUser?.userGames.find(userGame => userGame.gameId === game.id))
+        setFilteredGames(ownedGames)
+    }, [games, profileUser])
+
+
+
+    return (
+        <Container>
+            <Paper sx={{mt: 3, mb: 3}}>
+                <Typography variant="h3" fontWeight={"bold"} textAlign={"left"} padding={2}>
+                    {profileUser?.name}
+                </Typography>
+            </Paper>
+            <Typography fontWeight={"bold"} textAlign={"left"}>{profileUser?.name}'s Game Library</Typography>
+            <GameList games={games} setGames={setGames} filteredGames={filteredGames} setFilteredGames={setFilteredGames} user={profileUser} currentUser={currentUser} />
+        </Container>
+    )
 }
