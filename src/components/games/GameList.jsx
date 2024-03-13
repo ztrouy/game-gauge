17b9d7f5c0d94a9e95cbb982bc7812138727d7f9
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-13 09:25:59 -0500

    Transfered in rendering of list of games

---
+++ src/components/games/GameList.jsx
@@ -0,0 +1,40 @@
+import { Box } from "@mui/material"
+import { Game } from "./Game.jsx"
+import { useEffect } from "react"
+
+export const GameList = ({ games, setGames, filteredGames, setFilteredGames, currentUser }) => {
+    useEffect(() => {
+        const sortedGames = games.sort((firstGame, secondGame) => {
+            const firstGameName = firstGame.name.toUpperCase()
+            const secondGameName = secondGame.name.toUpperCase()
+
+            if (firstGameName < secondGameName) {
+                return -1
+            } else if (firstGameName > secondGameName) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+
+        setGames(sortedGames)
+        
+        if (setFilteredGames) {
+            setFilteredGames(games)
+        }
+    }, [games])
+    
+    return (
+        <Box 
+            display={"flex"} 
+            justifyContent={"center"}
+            flexDirection={"row"} 
+            flexWrap={"wrap"} 
+            gap={4}
+        >
+            {filteredGames.map(game => {
+                return <Game game={game} currentUser={currentUser} key={game.id} />
+            })}
+        </Box>
+    )
+}
