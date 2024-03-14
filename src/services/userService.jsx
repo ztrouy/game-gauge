ad307099aeb52f6c00698749562d9f21be303f47
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-14 13:21:43 -0500

    Added fetch to update a user in the database

--- src/services/userService.jsx
+++ src/services/userService.jsx
@@ -16,12 +16,24 @@ export const getUserById = (id) => {
     return fetch(`http://localhost:8088/users?id=${id}&_embed=userGames`).then(res => res.json())
 }
   
-export const createUser = (customer) => {
+export const createUser = (newUser) => {
     return fetch("http://localhost:8088/users", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(customer),
+        body: JSON.stringify(newUser),
     }).then((res) => res.json())
+}
+
+export const updateUser = (updatedUser) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedUser)
+    }
+
+    return fetch(`http://localhost:8088/users/${updatedUser.id}`, putOptions)
 }
