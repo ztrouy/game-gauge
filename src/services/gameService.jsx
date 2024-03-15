eec82a3559d0f1675f167ee7b136ecf865d8df64
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-15 09:30:07 -0500

    Added fetches for getting game by id, creating a game, and updating a game

--- src/services/gameService.jsx
+++ src/services/gameService.jsx
@@ -2,6 +2,34 @@ export const getAllGames = () => {
     return fetch("http://localhost:8088/games?_embed=gameGenres&_expand=type").then(res => res.json())
 }
 
+export const getGameById = (gameId) => {
+    return fetch(`http://localhost:8088/games?id=${gameId}&_embed=gameGenres`).then(res => res.json())
+}
+
+export const createGame = (gameObject) => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(gameObject)
+    }
+
+    return fetch("http://localhost:8088/games", postOptions)
+}
+
+export const updateGame = (modifiedGameObject) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(modifiedGameObject)
+    }
+
+    return fetch(`http://localhost:8088/games/${modifiedGameObject.id}`, putOptions)
+}
+
 export const createUserGame = (userGameObject) => {
     const postOptions = {
         method: "POST",
