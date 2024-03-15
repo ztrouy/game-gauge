be2b8015f9e0fead6dd7d6643d0783ca342f1635
Zachary Trouy <ztrouy@gmail.com>
2024-03-15 14:26:11 -0500

    Added logic to call group components for each group

--- src/components/groups/Groups.jsx
+++ src/components/groups/Groups.jsx
@@ -1,3 +1,34 @@
+import { Box, Container, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import { getAllGroups } from "../../services/groupService"
+import { Group } from "./Group"
+
 export const Groups = ({ currentUser }) => {
-    return (<>Groups Views</>)
+    const [groups, setGroups] = useState([])
+
+
+
+    useEffect(() => {
+        fetchGroups()
+    }, [])
+    
+
+
+    const fetchGroups = () => {
+        getAllGroups().then(groupsArray => {
+            setGroups(groupsArray)
+        })
+    }
+
+
+
+    return (
+        <Container>
+            <Box>
+                {groups.map(group => {
+                    return <Group group={group} currentUser={currentUser} />
+                })}
+            </Box>
+        </Container>
+    )
 }
