6da7bb3c1714b7fe15397cd85b21c36e1c65b0f4
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-18 12:12:14 -0500

    Added initial rendering and logic for group object

--- src/components/groups/Group.jsx
+++ src/components/groups/Group.jsx
@@ -1,29 +1,61 @@
-import { Box, Paper, Typography } from "@mui/material"
+import { Box, Button, Paper, Typography } from "@mui/material"
 import { useEffect, useState } from "react"
 import { getUserById } from "../../services/userService"
+import { useNavigate } from "react-router"
 
 export const Group = ({ group, currentUser }) => {
     const [members, setMembers] = useState([])
+    const [activeUser, setActiveUser] = useState({})
+    const [isInGroup, setIsInGroup] = useState(false)
     
+
 
     useEffect(() => {
         setMembers([])
         group.userGroups.map(userGroup => {
-            getUserById(userGroup.userId).then(userArray => {
-                const userObject = userArray[0]
+            getUserById(userGroup.userId).then(userObject => {
                 setMembers(members => [...members, userObject])
             })
         })
     }, [group])
 
 
+    useEffect(() => {
+        getUserById(currentUser.id).then(userObject => {
+            setActiveUser(userObject)
+        })
+    }, [currentUser])
+
+
+    useEffect(() => {
+
+    })
+
+
+
+    const navigate = useNavigate()
+
+
+    const handleOpen = () => {
+        navigate(`/groups/${group.id}`)
+    }
+
+
+
     return(
-        <Paper sx={{marginTop: 1, padding: 4}}>
+        <Paper elevation={3} sx={{marginTop: 2, padding: 4}}>
             <Typography variant="h4" textAlign={"left"} fontWeight={"bold"}>{group.name}</Typography>
-            <Box>
-                {members.map(member => {
-                    return <Typography textAlign={"left"}>{member.name}</Typography>
-                })}
+            <Box paddingTop={1} display={"flex"} flexDirection={"row"} justifyContent={"space-between"}>
+                <Box>
+                    {members.map(member => {
+                        return <Typography textAlign={"left"} key={member.id}>{member.name}</Typography>
+                    })}
+
+                </Box>
+                <Box display={"flex"} flexDirection={"row"} alignItems={"flex-end"}>
+                    <Button>Leave</Button>
+                    <Button variant="contained" onClick={handleOpen}>Open</Button>
+                </Box>
             </Box>
         </Paper>
     )
