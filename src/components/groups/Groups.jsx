3dc0189453b9ee909a5a71a4bf6c2400aaf35e2b
Zachary Trouy <ztrouy@gmail.com>
2024-03-18 13:21:02 -0500

    Passed fetch for list of groups as prop to GroupList

--- src/components/groups/Groups.jsx
+++ src/components/groups/Groups.jsx
@@ -2,15 +2,26 @@ import { Container } from "@mui/material"
 import { useEffect, useState } from "react"
 import { getAllGroups } from "../../services/groupService"
 import { GroupList } from "./GroupList.jsx"
+import { getUserById } from "../../services/userService.jsx"
 
 export const Groups = ({ currentUser }) => {
     const [groups, setGroups] = useState([])
+    const [activeUser, setActiveUser] = useState({})
 
 
 
     useEffect(() => {
         fetchGroups()
     }, [])
+
+
+    useEffect(() => {
+        if (currentUser.id) {
+            getUserById(currentUser.id).then(userObject => {
+                setActiveUser(userObject)
+            })
+        }
+    }, [currentUser])
     
 
 
@@ -24,7 +35,7 @@ export const Groups = ({ currentUser }) => {
 
     return (
         <Container>
-            <GroupList groups={groups} currentUser={currentUser} />
+            <GroupList groups={groups} fetchGroups={fetchGroups} activeUser={activeUser} currentUser={currentUser} />
         </Container>
     )
 }
