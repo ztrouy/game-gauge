2f01de6cd34068b5fe7102fe2c262e4479fc0f52
Zachary Trouy <ztrouy@gmail.com>
2024-03-18 13:45:41 -0500

    Imported fetch for active user, and passed as prop to Group

--- src/components/groups/GroupList.jsx
+++ src/components/groups/GroupList.jsx
@@ -1,11 +1,11 @@
 import { Box } from "@mui/material"
 import { Group } from "./Group.jsx"
 
-export const GroupList = ({ currentUser, activeUser, groups, fetchGroups}) => {
+export const GroupList = ({ currentUser, activeUser, fetchActiveUser, groups, fetchGroups}) => {
     return (
         <Box>
             {groups.map(group => {
-                return <Group group={group} fetchGroups={fetchGroups} currentUser={currentUser} activeUser={activeUser} key={group.id} />
+                return <Group group={group} fetchGroups={fetchGroups} currentUser={currentUser} activeUser={activeUser} fetchActiveUser={fetchActiveUser} key={group.id} />
             })}
         </Box>
     )
