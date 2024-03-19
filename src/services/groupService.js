ccd656b7be44287a975071f90c42784c3020ce5f
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-03-19 13:36:34 -0500

    Added fetches for getting a group by id, creating a group, and editing a group

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -2,6 +2,34 @@ export const getAllGroups = () => {
     return fetch("http://localhost:8088/groups?_embed=userGroups").then(res => res.json())
 }
 
+export const getGroupById = (groupId) => {
+    return fetch(`http://localhost:8088/groups/${groupId}`).then(res => res.json())
+}
+
+export const createGroup = (newGroup) => {
+    const postOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newGroup)
+    }
+
+    return fetch("http://localhost:8088/groups", postOptions)
+}
+
+export const updateGroup = (editedGroup) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(editedGroup)
+    }
+
+    return fetch(`http://localhost:8088/groups/${editedGroup.id}`, putOptions)
+}
+
 export const createUserGroup = (newUserGroup) => {
     const postOptions = {
         method: "POST",
