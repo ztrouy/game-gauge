38751c06cb36485b1534429b41c721b76ab800e4
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-01 08:41:58 -0500

    Added support for themes and mobile view

--- src/components/games/FilterBar.jsx
+++ src/components/games/FilterBar.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { getAllGameTypes } from "../../services/gameTypeService.js"
 import { getAllGenres } from "../../services/genreService.js"
-import { AppBar, Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Paper, Select, TextField, ToggleButton, ToggleButtonGroup, Toolbar } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, AppBar, Box, Button, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, MenuItem, Paper, Select, TextField, ToggleButton, ToggleButtonGroup, Toolbar, useTheme } from "@mui/material"
+import { ExpandMore, FilterAlt, Settings } from "@mui/icons-material"
 
 export const FilterBar = ({ games, setFilteredGames, user }) => {
     const [gameTypes, setGameTypes] = useState([])
@@ -13,6 +14,7 @@ export const FilterBar = ({ games, setFilteredGames, user }) => {
     const [titleFilter, setTitleFilter] = useState("")
     const [showOnlyOwnedGames, setShowOnlyOwnedGames] = useState(false)
     const [mobileFilterToDisplay, setMobileFilterToDisplay] = useState(0)
+    const [mobileFilterToggle, setMobileFilterToggle] = useState(false)
 
     
     
@@ -63,6 +65,9 @@ export const FilterBar = ({ games, setFilteredGames, user }) => {
 
 
 
+    const theme = useTheme()
+    
+    
     const clearFilters = () => {
         setShowOnlyOwnedGames(false)
         setGameTypeFilter(0)
@@ -88,12 +93,12 @@ export const FilterBar = ({ games, setFilteredGames, user }) => {
 
 
     return (
-        <AppBar position="static" sx={{marginBottom: 3}}>
+        <AppBar position="static" enableColorOnDark sx={{marginBottom: 3, backgroundColor: theme.palette.surface.default, backgroundImage: "none"}}>
             <Container maxWidth="x1">
                 <Toolbar disableGutters>
                     <Grid container direction={"row"} justifyContent={"space-between"} marginBottom={3} paddingTop={3}>
-                        <Box sx={{display: {xs: "none", md: "inline-block" }}}>
-                            <Grid container direction={"row"}>
+                        <Box sx={{display: {xs: "none", md: "inline-block"}}}>
+                            <Grid gap={1} container direction={"row"}>
                                 <FormControl sx={{width: 200}}>
                                     <InputLabel>Filter by Game Type</InputLabel>
                                     <Select
@@ -133,14 +138,14 @@ export const FilterBar = ({ games, setFilteredGames, user }) => {
                                         setTitleFilter(event.target.value)
                                     }}
                                 />
-                                <Button variant="outlined" onClick={clearFilters}>Clear Filters</Button>
+                                <Button variant="contained" onClick={clearFilters}>Clear Filters</Button>
                             </Grid>
                         </Box>
                         <ToggleButtonGroup
                             value={showOnlyOwnedGames}
                             exclusive
                             onChange={handleGamesListToggle}
-                            sx={{display: {xs: "none", md: "inline-block"}}}
+                            sx={{display: {xs: "none", md: "flex"}}}
                         >
                             <ToggleButton value={false}>
                                 All Games
@@ -149,90 +154,100 @@ export const FilterBar = ({ games, setFilteredGames, user }) => {
                                 My Games
                             </ToggleButton>
                         </ToggleButtonGroup>
-                        <ToggleButtonGroup
-                            value={showOnlyOwnedGames}
-                            exclusive
-                            onChange={handleGamesListToggle}
-                            fullWidth
-                            sx={{display: {md: "none"}}}
-                        >
-                            <ToggleButton value={false}>
-                                All Games
-                            </ToggleButton>
-                            <ToggleButton value={true}>
-                                My Games
-                            </ToggleButton>
-                        </ToggleButtonGroup>
-                        <ToggleButtonGroup
-                            exclusive
-                            fullWidth
-                            sx={{display: {md: "none"}, marginTop: 2}}
-                            value={mobileFilterToDisplay}
-                            onChange={handleMobileFilter}
-                        >
-                            <ToggleButton value={1}>Type</ToggleButton>
-                            <ToggleButton value={2}>Genre</ToggleButton>
-                            <ToggleButton value={3}>Title</ToggleButton>
-                        </ToggleButtonGroup>
-                        {mobileFilterToDisplay !== 0 ? (
-                            <Box sx={{display: {xs: "block", md: "none"}, width: "100%", marginTop: 3}}>
-                                {mobileFilterToDisplay === 1 ? (
-                                    <FormControl fullWidth>
-                                        <InputLabel>Filter by Game Type</InputLabel>
-                                        <Select
-                                            value={gameTypeFilter}
-                                            label="Filter by Game Type"
-                                            onChange={(event) => {
-                                                setGameTypeFilter(event.target.value)
-                                            }}
-                                        >
-                                            <MenuItem value={0} key={0}>Select a Game Type</MenuItem>
-                                            {gameTypes.map(gameType => {
-                                                return <MenuItem value={gameType.id} key={gameType.id}>{gameType.name}</MenuItem>
-                                            })}
-                                        </Select>
-                                    </FormControl>
-                                ) : (
-                                    ""
-                                )}
-                                {mobileFilterToDisplay === 2 ? (
-                                    <FormControl fullWidth>
-                                        <InputLabel>Filter by Genre</InputLabel>
-                                        <Select
-                                            value={genreFilter}
-                                            label="Filter by Genre"
-                                            fullWidth
-                                            onChange={(event) => {
-                                                setGenreFilter(event.target.value)
-                                            }}
-                                        >
-                                            <MenuItem value={0} key={0}>Select a Genre</MenuItem>
-                                            {genres.map(genre => {
-                                                return <MenuItem value={genre.id} key={genre.id}>{genre.genre}</MenuItem>
-                                            })}
-                                        </Select>
-                                    </FormControl>
-                                ) : (
-                                    ""
-                                )}
-                                {mobileFilterToDisplay === 3 ? (
-                                    <TextField 
-                                        id="outlined-search"
-                                        label="Search for a Game"
-                                        type="search"
+                        <Container disableGutters sx={{display: {xs: "block", md: "none"}}}>
+                            <TextField 
+                                id="outlined-search"
+                                label="Search for a Game"
+                                type="search"
+                                value={titleFilter}
+                                onChange={(event) => {
+                                    setTitleFilter(event.target.value)
+                                }}
+                                fullWidth 
+                                InputProps={{
+                                    endAdornment: (
+                                        <IconButton onClick={() => setMobileFilterToggle(!mobileFilterToggle)}>
+                                            <FilterAlt />
+                                        </IconButton>
+                                    )
+                                }}
+                            />
+                            {mobileFilterToggle ? (
+                                <Container disableGutters sx={{marginTop: 2}}>
+                                    <ToggleButtonGroup
+                                        value={showOnlyOwnedGames}
+                                        exclusive
+                                        onChange={handleGamesListToggle}
                                         fullWidth
-                                        value={titleFilter}
-                                        onChange={(event) => {
-                                            setTitleFilter(event.target.value)
-                                        }}
-                                    />
-                                ) : (
-                                    ""
-                                )}
-                            </Box>
-                        ) : (
-                            ""
-                        )}
+                                        sx={{display: {md: "none"}}}
+                                    >
+                                        <ToggleButton value={false}>
+                                            All Games
+                                        </ToggleButton>
+                                        <ToggleButton value={true}>
+                                            My Games
+                                        </ToggleButton>
+                                    </ToggleButtonGroup>
+                                    <ToggleButtonGroup
+                                        exclusive
+                                        fullWidth
+                                        sx={{display: {md: "none"}, marginTop: 2}}
+                                        value={mobileFilterToDisplay}
+                                        onChange={handleMobileFilter}
+                                    >
+                                        <ToggleButton value={1}>Type</ToggleButton>
+                                        <ToggleButton value={2}>Genre</ToggleButton>
+                                    </ToggleButtonGroup>
+                                    {mobileFilterToDisplay !== 0 ? (
+                                        <Box sx={{display: {xs: "block", md: "none"}, width: "100%", marginTop: 3}}>
+                                            {mobileFilterToDisplay === 1 ? (
+                                                <FormControl fullWidth>
+                                                    <InputLabel>Filter by Game Type</InputLabel>
+                                                    <Select
+                                                        value={gameTypeFilter}
+                                                        label="Filter by Game Type"
+                                                        onChange={(event) => {
+                                                            setGameTypeFilter(event.target.value)
+                                                        }}
+                                                    >
+                                                        <MenuItem value={0} key={0}>Select a Game Type</MenuItem>
+                                                        {gameTypes.map(gameType => {
+                                                            return <MenuItem value={gameType.id} key={gameType.id}>{gameType.name}</MenuItem>
+                                                        })}
+                                                    </Select>
+                                                </FormControl>
+                                            ) : (
+                                                ""
+                                            )}
+                                            {mobileFilterToDisplay === 2 ? (
+                                                <FormControl fullWidth>
+                                                    <InputLabel>Filter by Genre</InputLabel>
+                                                    <Select
+                                                        value={genreFilter}
+                                                        label="Filter by Genre"
+                                                        fullWidth
+                                                        onChange={(event) => {
+                                                            setGenreFilter(event.target.value)
+                                                        }}
+                                                    >
+                                                        <MenuItem value={0} key={0}>Select a Genre</MenuItem>
+                                                        {genres.map(genre => {
+                                                            return <MenuItem value={genre.id} key={genre.id}>{genre.genre}</MenuItem>
+                                                        })}
+                                                    </Select>
+                                                </FormControl>
+                                            ) : (
+                                                ""
+                                            )}
+                                        </Box>
+                                    ) : (
+                                        ""
+                                    )}
+                                </Container>
+                            ): (
+                                ""
+                            )}
+                        </Container>
                     </Grid>
                 </Toolbar>
             </Container>
