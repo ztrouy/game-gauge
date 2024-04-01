66b2e8d2f4a67d96b742e1d5a8f55bd3bf67a5a9
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-01 08:41:25 -0500

    Added mobile view for isCompact view, and added support for themes

--- src/components/games/Game.jsx
+++ src/components/games/Game.jsx
@@ -1,8 +1,9 @@
-import { Button, Card, CardMedia, Typography, Box, Paper } from "@mui/material"
+import { Button, Card, CardMedia, Typography, Box, Paper, useTheme, IconButton } from "@mui/material"
 import { useEffect, useState } from "react"
 import { getUserById } from "../../services/userService.jsx"
 import { createUserGame, deleteUserGame } from "../../services/gameService.jsx"
 import { Link } from "react-router-dom"
+import { Delete, Edit } from "@mui/icons-material"
 
 
 export const Game = ({ game, isCompact, user, fetchUserData, currentUser }) => {
@@ -34,6 +35,9 @@ export const Game = ({ game, isCompact, user, fetchUserData, currentUser }) => {
 
 
 
+    const theme = useTheme()
+
+    
     const handleButton = () => {
         if (ownsGame) {
             const currentUserGame = activeUser.userGames.find(userGame => userGame.gameId === game.id)
@@ -56,10 +60,10 @@ export const Game = ({ game, isCompact, user, fetchUserData, currentUser }) => {
     return (
         <>
             {isCompact ? (
-                <Paper sx={{width: "100%"}}>
+                <Paper sx={{width: "100%", backgroundImage: "none", backgroundColor: theme.palette.surface.default}}>
                     <Box display={"flex"} flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"} padding={2}>
                         <Typography fontWeight={"bold"}>{game.name}</Typography>
-                        <Box sx={{ display: "flex", justifyContent: "right"}}>
+                        <Box sx={{ display: {xs: "none", md: "flex"}, justifyContent: "right"}}>
                             {currentUser.isStaff ? (
                                 <Button variant="contained" sx={{marginRight: 1, color: "white"}} component={Link} to={`/games/edit/${game.id}`}>Edit Game</Button>
                             ) : (
@@ -71,11 +75,27 @@ export const Game = ({ game, isCompact, user, fetchUserData, currentUser }) => {
                                 <Button variant="contained" onClick={handleButton}>Add Game</Button>
                             )}
                         </Box>
+                        <Box sx={{ display: {xs: "flex", md: "none"}, justifyContent: "right"}}>
+                            {currentUser.isStaff ? (
+                                <IconButton component={Link} to={`/games/edit/${game.id}`}>
+                                    <Edit />
+                                </IconButton>
+                            ) : (
+                                ""
+                            )}
+                            {ownsGame ? (
+                                <IconButton onClick={handleButton}>
+                                    <Delete />
+                                </IconButton>
+                            ) : (
+                                ""
+                            )}
+                        </Box>
                     </Box>
                 </Paper>
             ) : (
                 <Paper margin={2}>
-                    <Card sx={{width: 350}}>
+                    <Card width={350} sx={{backgroundImage: "none", backgroundColor: theme.palette.surface.default}}>
                         <CardMedia 
                             component={"img"}
                             sx={{height: 165}}
